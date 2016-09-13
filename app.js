390ef626087a18254ea45db431265b08115a0e95
sirinya <nutsuke.m@gmail.com>
2016-09-13 14:21:21 +0700

    add auth to project

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var db = require('./database');
+var auth = require('./auth');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,12 @@ var users = require('./routes/users');
 var sys_report = require('./routes/sys_reportController');
 
 var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,7 +41,7 @@ app.use(function (req, res, next) {
 app.use('/', routes);
 app.use('/users', users);
 
-app.use('/sys_report', sys_report);
+app.use('/sys_report', auth, sys_report);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/sys_reportController.js
+++ routes/sys_reportController.js
@@ -4,18 +4,13 @@ var router = express.Router();
 var Sys_reportDao = require('../models/sys_reportDao');
 var Sys_reportService = require('../services/sys_reportService');
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource: sys_report');
-});
-
 router.post('/getSys_reportByIdAndYear', function (req, res) {
   var db = req.db;
   var id = req.body.id; 
   var year = req.body.year; 
   Sys_reportDao.getSys_reportById(db, id)
     .then(function (rows) {
-      // succes
-
+      
       var tableName = rows[0].source_table;
 
       Sys_reportService.getResultByYear(db, tableName, year)
@@ -28,7 +23,6 @@ router.post('/getSys_reportByIdAndYear', function (req, res) {
 
     })
     .catch(function (err) {
-      // error
       res.send({ ok: false, msg: err });
     });
   
